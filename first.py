0aaa00e506dda953b0f0b43881f02543515017d7
salekeennayeem <nawaz.nayeem@selise.ch>
2023-12-12 14:54:06 +0600

    init commit

---
+++ first.py
@@ -0,0 +1,9 @@
+class Solution:
+    @staticmethod
+    def mergeAlternately(word1: str, word2: str) -> str:
+        return word1 + word2
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.mergeAlternately(word1="ayo", word2="yo"))
