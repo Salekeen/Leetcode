8a89595859cad49a6d84cf9f4ef22da8fff46c75
salekeennayeem <nawaz.nayeem@selise.ch>
2023-12-12 17:29:59 +0600

    1768 done

--- first.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    @staticmethod
-    def mergeAlternately(word1: str, word2: str) -> str:
-        return word1 + word2
-
-
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.mergeAlternately(word1="ayo", word2="yo"))
---
+++ leetcode_1768.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        result = []
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+
+        result.append(word1[i:])
+        result.append(word2[j:])
+        return ''.join(result)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.mergeAlternately(word1="ab", word2="pqrs"))
