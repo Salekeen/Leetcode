a5b51f8db26a358e4773b3b49178063765c5b3be
salekeennayeem <nawaz.nayeem@selise.ch>
2023-12-12 17:32:30 +0600

    started 283

---
+++ leetcode_283.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def moveZeroes(nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        return None
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    solution.moveZeroes(nums=[0, 1, 0, 3, 12])
