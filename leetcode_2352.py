bfdfc8d57be5eb9aafa2e52aac1a123f0c516978
Salekeen <nawazsalekeennayeem@gmail.com>
2023-12-15 08:57:46 +0600

    2352 done

--- leetcode_2352.py
+++ leetcode_2352.py
@@ -2,10 +2,26 @@ from typing import List
 
 
 class Solution:
-    def equalPairs(self, grid: List[List[int]]) -> int:
-        pass
+    @staticmethod
+    def equalPairs(grid: List[List[int]]) -> int:
+        count = 0
+        columns_map = {}
+        for row in grid:
+            for index, column in enumerate(row):
+                columns_map.setdefault(index, []).append(column)
+
+        for row in grid:
+            for i in range(len(row)):
+                matched = True
+                for j in range(len(row)):
+                    if columns_map[i][j] != row[j]:
+                        matched = False
+                        break
+                if matched:
+                    count += 1
+        return count
 
 
 if __name__ == '__main__':
     solution = Solution()
-    solution.equalPairs([[3, 2, 1], [1, 7, 6], [2, 7, 7]])
+    print(solution.equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
