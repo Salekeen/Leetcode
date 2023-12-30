3ab8b1327987a832048e8013fbe9b5f3d1712b3a
Salekeen <nawazsalekeennayeem@gmail.com>
2023-12-30 16:44:48 +0600

    1679 done

--- leetcode_1679.py
+++ leetcode_1679.py
@@ -1,23 +1,26 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     @staticmethod
     def maxOperations(nums: List[int], k: int) -> int:
         op_count = 0
-        mappings = {}
-
+        counter = Counter(nums)
         for i in range(len(nums)):
             y = k - nums[i]
-            if mappings.get(y) is None:
-                mappings[nums[i]] = i
-            else:
-                op_count += 1
-                del mappings[y]
+            if counter.get(y) is not None:
+                if not nums[i] == (k / 2):
+                    op_count += min(counter.get(y), counter.get(nums[i]))
+                else:
+                    op_count += (counter[nums[i]] // 2)
+
+                del counter[nums[i]]
+                del counter[y]
 
         return op_count
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.maxOperations([2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2], 3))
+    print(solution.maxOperations([3, 1, 3, 4, 3], 6))
