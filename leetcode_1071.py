14d226e312604551f3267835a4ef7dd61b3c4340
Salekeen <nawazsalekeennayeem@gmail.com>
2024-01-03 22:18:03 +0600

    1071 done

--- leetcode_1071.py
+++ leetcode_1071.py
@@ -1,13 +1,20 @@
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
+    @staticmethod
+    def gcdOfStrings(str1: str, str2: str) -> str:
         output_string = ""
         # let str1 represent the shorter str
         if len(str1) > len(str2):
             str1, str2 = str2, str1
         len_str_1 = len(str1)
         for i in range(len(str1), 0, -1):
-            if i % len_str_1 == 0 and i % len(str2) == 0:
-                pass
+            if len_str_1 % i == 0:
+                multiplier_1 = len(str1) // i
+                multiplier_2 = len(str2) // i
+                if str1[:i] * multiplier_1 == str1 and str1[:i] * multiplier_2 == str2:
+                    output_string += str1[:i]
+                    break
+
+        return output_string
 
 
 if __name__ == '__main__':
